725a794603c859f2d06eed4cf2406c93aa93b5c2
Matheus Bett <104926469+mtbetterr@users.noreply.github.com>
2022-05-24 12:22:09 -0300

    Poo

---
+++ pooProject/Alunos/Alunos.java
@@ -0,0 +1,18 @@
+package Alunos;
+public class Alunos{
+    public static void main(String[] args) {
+        AlunosClass A1 = new AlunosClass();
+        A1.nome = "matheus";
+        A1.nota = 10f;
+        A1.status();
+        A1.idade = 19;
+
+        AlunosClass A2 = new AlunosClass();
+        A2.nome= "julio";
+        A2.nota = 5f;
+        A2.status();
+        A2.idade = 22;
+
+    }
+       
+}
---
+++ pooProject/gabarito/questoes.java
@@ -0,0 +1,15 @@
+package gabarito;
+
+public class questoes {
+    String questResp;
+    char resp1;
+    char resp2;
+    char resp3;
+    char resp4;
+    char resp5;
+    char resp6;
+    char resp7;
+    char resp8;
+    char resp9;
+    char resp10;
+}
